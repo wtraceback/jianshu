cbc1ef29f6e6eb931d7fa578ff9924f27d04d3e3
wtraceback <whxcer@outlook.com>
2021-07-04 11:51:41 +0800

    在 React 中使用路由功能的初始尝试

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import Header from './common/header'
 import store from './store'
@@ -8,7 +9,15 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header />
+        <>
+          <Header />
+          <BrowserRouter>
+            <div style={{marginTop: "56px",}}>
+              <Route path="/" exact render={() => <div>Home</div>}></Route>
+              <Route path="/detail" exact render={() => <div>Detail</div>}></Route>
+            </div>
+          </BrowserRouter>
+        </>
       </Provider>
     )
   }
