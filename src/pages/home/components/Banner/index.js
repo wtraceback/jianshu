9b250ac4cd53c6f03bbfe9c8366c273dec84d0c4
wtraceback <whxcer@outlook.com>
2021-07-04 23:50:54 +0800

    首页横幅图片栏组件的完成

--- src/pages/home/components/Banner/index.js
+++ src/pages/home/components/Banner/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
+import styles from './index.module.css'
+import bannerImg from '../../../../statics/slam_dunk.jpg';
+
 class Banner extends Component {
     render() {
         return (
-            <div>
-                Banner
+            <div className={`${styles.carousel} ${styles.slide}`}>
+                <div className={styles.carousel_inner}>
+                    <div className={styles.banner} data-banner-name="首页图片栏">
+                        <a target="_blank" href="/">
+                            <img className={styles.banner_img} src={bannerImg} alt="540" />
+                        </a>
+                    </div>
+                </div>
             </div>
         )
     }
