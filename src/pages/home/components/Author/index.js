2f66af736cc95d1731458a9ea3fc02bc2b924d21
wtraceback <whxcer@outlook.com>
2021-07-13 07:39:44 +0800

    推荐作者 组件的分页显示

--- src/pages/home/components/Author/index.js
+++ src/pages/home/components/Author/index.js
@@ -1,96 +1,55 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import styles from './index.module.css'
 
 class Writer extends Component {
     render() {
+        let { authors_page, authors_perpage, recommend_authors_list } = this.props;
+        let list = recommend_authors_list.toJS()
+        let list_len = list.length
+        let totalpage = Math.ceil(list_len / authors_perpage)
+        let page_list = []
+
+        for(let i = authors_page * authors_perpage; i < list_len && i < authors_perpage + authors_page * authors_perpage; i++) {
+            page_list.push(list[i])
+        }
+
         return (
             <div className={styles.author_warp}>
                 <div className={styles.authors}>
                     <div className={styles.title}>
                         <span>推荐作者</span>
-                        <a className={styles.page_change} href="/">
+                        <a className={styles.page_change} href="/" onClick={this.props.handleAuthorPageChange}>
                             <i className={`iconfont icon-spin`}></i>
                             换一批
                         </a>
                     </div>
+
                     <ul className={styles.list}>
-                        <li>
-                            <a href="/u/ef4f2422125f?utm_source=desktop&amp;utm_medium=index-users" target="_blank" className={styles.avatar}>
-                                <img src="https://upload.jianshu.io/users/upload_avatars/301940/189d69dd-af7c-4290-9e2c-89e98acf3603.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                            </a> 
-                            <a className={styles.follow} state="0" href="/">
-                                <i className="iconfont icon-follow"></i>
-                                关注
-                            </a> 
-                            <a href="/u/ef4f2422125f?utm_source=desktop&amp;utm_medium=index-users" target="_blank" className={styles.name}>
-                                卢璐说
-                            </a> 
-                            <p>
-                                写了2055.6k字 · 34.3k喜欢
-                            </p>
-                        </li>
-                        <li>
-                            <a href="/u/ef4f2422125f?utm_source=desktop&amp;utm_medium=index-users" target="_blank" className={styles.avatar}>
-                                <img src="https://upload.jianshu.io/users/upload_avatars/301940/189d69dd-af7c-4290-9e2c-89e98acf3603.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                            </a> 
-                            <a className={styles.follow} state="0">
-                                <i className="iconfont icon-follow"></i>
-                                关注
-                            </a> 
-                            <a href="/u/ef4f2422125f?utm_source=desktop&amp;utm_medium=index-users" target="_blank" className={styles.name}>
-                                卢璐说
-                            </a> 
-                            <p>
-                                写了2055.6k字 · 34.3k喜欢
-                            </p>
-                        </li>
-                        <li>
-                            <a href="/u/ef4f2422125f?utm_source=desktop&amp;utm_medium=index-users" target="_blank" className={styles.avatar}>
-                                <img src="https://upload.jianshu.io/users/upload_avatars/301940/189d69dd-af7c-4290-9e2c-89e98acf3603.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                            </a> 
-                            <a className={styles.follow} state="0">
-                                <i className="iconfont icon-follow"></i>
-                                关注
-                            </a> 
-                            <a href="/u/ef4f2422125f?utm_source=desktop&amp;utm_medium=index-users" target="_blank" className={styles.name}>
-                                卢璐说
-                            </a> 
-                            <p>
-                                写了2055.6k字 · 34.3k喜欢
-                            </p>
-                        </li>
-                        <li>
-                            <a href="/u/ef4f2422125f?utm_source=desktop&amp;utm_medium=index-users" target="_blank" className={styles.avatar}>
-                                <img src="https://upload.jianshu.io/users/upload_avatars/301940/189d69dd-af7c-4290-9e2c-89e98acf3603.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                            </a> 
-                            <a className={styles.follow} state="0">
-                                <i className="iconfont icon-follow"></i>
-                                关注
-                            </a> 
-                            <a href="/u/ef4f2422125f?utm_source=desktop&amp;utm_medium=index-users" target="_blank" className={styles.name}>
-                                卢璐说
-                            </a> 
-                            <p>
-                                写了2055.6k字 · 34.3k喜欢
-                            </p>
-                        </li>
-                        <li>
-                            <a href="/u/ef4f2422125f?utm_source=desktop&amp;utm_medium=index-users" target="_blank" className={styles.avatar}>
-                                <img src="https://upload.jianshu.io/users/upload_avatars/301940/189d69dd-af7c-4290-9e2c-89e98acf3603.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                            </a> 
-                            <a className={styles.follow} state="0">
-                                <i className="iconfont icon-follow"></i>
-                                关注
-                            </a> 
-                            <a href="/u/ef4f2422125f?utm_source=desktop&amp;utm_medium=index-users" target="_blank" className={styles.name}>
-                                卢璐说
-                            </a> 
-                            <p>
-                                写了2055.6k字 · 34.3k喜欢
-                            </p>
-                        </li>
+                        {
+                            page_list.map((item) => {
+                                return (
+                                    <li key={item.id}>
+                                        <a href="/" target="_blank" className={styles.avatar}>
+                                            <img src={item.avatarUrl} alt="avatar" />
+                                        </a>
+                                        <a className={styles.follow} state="0" href="/">
+                                            <i className="iconfont icon-follow"></i>
+                                            关注
+                                        </a>
+                                        <a href="/u/ef4f2422125f?utm_source=desktop&amp;utm_medium=index-users" target="_blank" className={styles.name}>
+                                            {item.author}
+                                        </a>
+                                        <p>
+                                            写了{item.words}字 · {item.like}喜欢
+                                        </p>
+                                    </li>
+                                )
+                            })
+                        }
                     </ul>
+
                     <a href="/" target="_blank" className={styles.find_more}>
                         查看全部
                         <i className="iconfont icon-dayuhao1"></i>
@@ -101,4 +60,21 @@ class Writer extends Component {
     }
 }
 
-export default Writer
+const mapStateToProps = (state) => {
+    return {
+        recommend_authors_list: state.get('home').get('recommend_authors_list'),
+        authors_page: state.get('home').get('authors_page'),
+        authors_perpage: state.get('home').get('authors_perpage'),
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleAuthorPageChange(e) {
+            e.preventDefault()
+            console.log('test')
+        },
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Writer)
